3acfcb30e970bce3f14032b4ca5ad17620b5073f
weilunlee <zorro.weilee@gmail.com>
2024-04-14 19:52:24 +0800

    新增：初始化

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Calander from './Calander/Calander';
+
+function App() {
+  return (
+    <div className="App">
+      <Calander />
+    </div>
+  );
+}
+
+export default App;
